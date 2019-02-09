7cdd1a41f33e540324ea2946d8a5001c4602be91
tbvjaos510 <tbvjaos510@naver.com>
2019-02-09 20:17:01 +0900

    [CHANGE] 정보 불러오기 방법 변경

--- python/source/youtube.py
+++ python/source/youtube.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import pafy
 import urllib
 import pyperclip
+import time
 from pprint import pprint as pp
 import simplejson as json
+
+
 class youtube():
 
     NOT_YOUTUBE = 0
@@ -12,8 +17,7 @@ class youtube():
     CHANNEL = 4
     OTHERS = 5
 
-    API_KEY =""
-
+    API_KEY = ""
 
     def __init__(self, key):
         self.API_KEY = key
@@ -26,7 +30,7 @@ class youtube():
             sub_url = splited[1]
             if sub_url.find("playlist") == 0:
                 return self.PLAYLIST
-            elif sub_url.find("watch") ==0 :
+            elif sub_url.find("watch") == 0:
                 if sub_url.find("&list=") == -1:
                     return self.WATCH
                 else:
@@ -45,15 +49,14 @@ class youtube():
             return url
         elif urltype == self.PLAYLIST:
             id = url[url.find("list=")+len("list="):].split('&')[0]
-            return self.get_videos_in_list(id,True)
+            return self.get_videos_in_list(id, True)
         elif urltype == self.WATCH_AND_PLAYLIST:
             id = url[url.find("list=")+len("list="):].split('&')[0]
-            return self.get_videos_in_list(id,True)
+            return self.get_videos_in_list(id, True)
 
         elif urltype == self.CHANNEL:
-            id =url[url.find("channel/")+len("channel/"):].split('/')[0]
-            return self.get_videos_in_channel(id,True)
-
+            id = url[url.find("channel/")+len("channel/"):].split('/')[0]
+            return self.get_videos_in_channel(id, True)
 
         elif urltype == self.OTHERS:
             return None
@@ -61,25 +64,27 @@ class youtube():
         # except:
         #     return None
 
-    def get_videos_in_channel(self,channel_id,print_title=False):
+    def get_videos_in_channel(self, channel_id, print_title=False):
 
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
-        first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(self.API_KEY, channel_id)
-        
+        first_url = base_search_url + \
+            'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(
+                self.API_KEY, channel_id)
+
         video_links = []
         url = first_url
-        print("API URL : ",url)
+        print("API URL : ", url)
         while True:
             inp = urllib.request.urlopen(url)
             resp = json.load(inp)
 
             for i in resp['items']:
                 if i['id']['kind'] == "youtube#video":
-                    if i['snippet']['title']!="Private video" and i['snippet']["liveBroadcastContent"] == 'none':
+                    if i['snippet']['title'] != "Private video" and i['snippet']["liveBroadcastContent"] == 'none':
                         video_links.append(base_video_url + i['id']['videoId'])
-    
+
             try:
                 next_page_token = resp['nextPageToken']
                 url = first_url + '&pageToken={}'.format(next_page_token)
@@ -87,16 +92,18 @@ class youtube():
                 break
         return video_links
 
-    def get_videos_in_list(self,list_id, print_title=False):
+    def get_videos_in_list(self, list_id, print_title=False):
 
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/playlistItems?'
 
-        first_url = base_search_url+'key={}&playlistId={}&part=snippet,id&order=date&maxResults=25'.format(self.API_KEY, list_id)
-        
+        first_url = base_search_url + \
+            'key={}&playlistId={}&part=snippet,id&order=date&maxResults=25'.format(
+                self.API_KEY, list_id)
+
         video_links = []
         url = first_url
-        print("API URL : ",url)
+        print("API URL : ", url)
         while True:
             inp = urllib.request.urlopen(url)
             resp = json.load(inp)
@@ -104,14 +111,43 @@ class youtube():
             for i in resp['items']:
                 i = i['snippet']
                 if i['resourceId']['kind'] == "youtube#video":
-                    if i['title']!="Private video":
-                        video_links.append(base_video_url + i['resourceId']['videoId'])
+                    if i['title'] != "Private video":
+                        video_links.append({
+                            url: base_video_url + i['resourceId']['videoId']),
+                            title: i['title'].
+                            thumbnail: i['thumbnails']['default']['url']
+                        }
             try:
                 next_page_token = resp['nextPageToken']
                 url = first_url + '&pageToken={}'.format(next_page_token)
             except:
                 break
+            time.sleep(0.1)
         return video_links
+
+    def getVideoId(self, url):
+        return url[url.find("v=")+len("v="):].split('&')[0]
+
+    def getVideoinfo(self, url):
+        print(url)
+
+        base_video_url = 'https://www.googleapis.com/youtube/v3/videos?id={}&key={}&part=snippet,contentDetails'.format(
+            self.getVideoId(url), self.API_KEY)
+        inp = urllib.request.urlopen(base_video_url)
+        resp = json.load(inp)
+
+        for i in resp['items']:
+            if i['kind'] == "youtube#video":
+                if i['snippet']['title'] != "Private video":
+                    # print(i['snippet']['title'])
+                    # print(i['snippet']['thumbnails'])
+                    # print(i["contentDetails"])
+                    return {
+                        "thumbnail": i['snippet']['thumbnails']['default']['url'],
+                        "title": i['snippet']['title'],
+                        "duration": i['contentDetails']['duration']
+                    }
+        return None
 
     def getInfo(self, url, ext_filter=None, resl_filter=None):
         print(url)
@@ -124,13 +160,12 @@ class youtube():
         ytube_info = {}
 
         if ext_filter is None and resl_filter is None:
-        
-            for s in ytube_video.streams:
-                
-                ext = s.extension # 확장자 
-                ytube_ext_info = [s.url, s.get_filesize()] # url과 파일 사이즈
-                ytube_ext_info_dic = {s.resolution : ytube_ext_info}
 
+            for s in ytube_video.streams:
+
+                ext = s.extension  # 확장자
+                ytube_ext_info = [s.url, s.get_filesize()]  # url과 파일 사이즈
+                ytube_ext_info_dic = {s.resolution: ytube_ext_info}
 
                 if not ext in ytube_exts:
 
@@ -140,11 +175,11 @@ class youtube():
                 ytube_exts[ext].append(ytube_ext_info_dic)
 
             ytube_info = {
-            "thumbnail" : ytube_thumbnail,
-            "title" : ytube_video.title,
-            "download" : ytube_exts,
-            "duration" : ytube_duration
-        }
+                "thumbnail": ytube_thumbnail,
+                "title": ytube_video.title,
+                "download": ytube_exts,
+                "duration": ytube_duration
+            }
 
         elif ext_filter is not None and resl_filter is not None:
 
@@ -156,14 +191,14 @@ class youtube():
 
                 if ext == ext_filter and s.resolution == resl_filter:
 
-                    ytube_ext_info = [s.url, s.get_filesize()] # url과 파일 사이즈
-                    ytube_ext_info_dic = {s.resolution : ytube_ext_info}
+                    ytube_ext_info = [s.url, s.get_filesize()]  # url과 파일 사이즈
+                    ytube_ext_info_dic = {s.resolution: ytube_ext_info}
 
                     ytube_info = {
-                    "thumbnail" : ytube_thumbnail,
-                    "title" : ytube_video.title,
-                    "download" : ytube_ext_info_dic,
-                    "duration" : ytube_duration
+                        "thumbnail": ytube_thumbnail,
+                        "title": ytube_video.title,
+                        "download": ytube_ext_info_dic,
+                        "duration": ytube_duration
                     }
                     EXIST = 1
 
@@ -172,15 +207,14 @@ class youtube():
                 ytube_best = ytube_video.getbest(preftype=ext_filter)
 
                 ytube_ext_info = [ytube_best.url, ytube_best.get_filesize()]
-                ytube_ext_info_dic = {ytube_best.resolution : ytube_ext_info}
+                ytube_ext_info_dic = {ytube_best.resolution: ytube_ext_info}
 
                 ytube_info = {
-                    "thumbnail" : ytube_thumbnail,
-                    "title" : ytube_video.title,
-                    "download" : ytube_ext_info_dic,
-                    "duration" : ytube_duration
-                    }
-        
+                    "thumbnail": ytube_thumbnail,
+                    "title": ytube_video.title,
+                    "download": ytube_ext_info_dic,
+                    "duration": ytube_duration
+                }
 
         return ytube_info
 
@@ -190,15 +224,17 @@ class youtube():
         if urlList is None:
             return None
         elif not isinstance(urlList, list):
-            return self.getInfo(url)
+            self.getVideoinfo(url)
 
-        InfoList = []
+        # InfoList = []
 
         for url in urlList:
-            InfoList.append(self.getInfo(url, ext, resl))
+            # InfoList.append(self.getInfo(url, ext, resl))
+            self.getVideoinfo(url)
+            time.sleep(0.01)
+        # return InfoList
 
-        return InfoList
 
-y = youtube("AIzaSyDm-jHPC3JX_4T8VMEkS5PdICMh-AUaWnY")
-#pp(y.getInfo(pyperclip.paste()))
-print(y.getInfos(pyperclip.paste(),"mp4","1280x720"))
+y = youtube("AIzaSyCmksLiLUv1yrxdnF1bPHt0x_O8wxwlGoE")
+# pp(y.getInfo(pyperclip.paste()))
+print(y.getInfos(pyperclip.paste(), "mp4", "1280x720"))

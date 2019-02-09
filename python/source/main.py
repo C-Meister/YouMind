82e04b1c87f7fb3d582df0d1c839759fefc78e09
ShinSH0 <klkura1@naver.com>
2019-02-09 23:34:34 +0900

    [CHANGE] UI 연결

---
+++ python/source/main.py
@@ -0,0 +1,28 @@
+import sys
+
+from Ui_test2 import *
+from kit_class import *
+from youtube import *
+
+
+
+downloaddata = []
+API_KEY = "AIzaSyDFC_QxH093_VthlLPWvC2BmzPP0hhbX9U"
+
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+
+MainWindow.show()
+
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+
+yt = youtube(API_KEY)
+
+list1 = Kit(ui)
+list1.add_download_list(downloaddata)
+
+app.exec_()
